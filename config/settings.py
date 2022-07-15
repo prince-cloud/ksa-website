561b46794b45a531918a0c9bba62a7317f1e2ce2
prince-cloud <acheampongprince227@gmail.com>
2022-07-14 17:16:04 -0700

    changed template dir

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
